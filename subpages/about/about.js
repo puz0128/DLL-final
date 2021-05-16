65275f3f280a5f45d18076485e63c01a6bebb873
puz0128 <70727939+puz0128@users.noreply.github.com>
2021-05-16 14:51:04 -0500

    Update about.js

--- subpages/about/about.js
+++ subpages/about/about.js
@@ -1,1 +1,3 @@
 /*function show() { document.getElementById('box').style.display = 'block'; }*/
+var name = prompt("What's your name?");
+document.getElementById('box').innerHTML=("CONGRATULATIONS "+ name +", YOU HAVE PASSED");
